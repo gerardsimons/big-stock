db7b300f694aac55c28b46a1b52e1e3f11156f41
gerardsimons <sjrasimons@gmail.com>
2016-02-22 23:19:01 +0100

    realtime downloader seems to work, historical is broken still...

--- rt_downloader.py
+++ rt_downloader.py
@@ -4,9 +4,10 @@ Download real-time stock data from the Yahoo Finance API
 import urllib
 import time
 
-SYMBOL_FILE_NAME = "NYSE.txt"
+# SYMBOL_FILE_NAME = "NYSE.txt"
+SYMBOL_FILE_NAME = "/media/gerardsimons/WD/Documents/Big Stock/downloader/data/historical/proper/symbols.txt"
 URL_FMT = "http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s"
-OUTPUT_FILE_FMT = '/media/gerardsimons/WD/Documents/Big Stock/downloader/data/realtime/out%d.csv'
+OUTPUT_FILE_FMT = '/media/gerardsimons/WD/Documents/Big Stock/downloader/data/realtime/round=%d_chunk=%d.csv'
 MAX_SYMBOLS = 10
 
 class RTDownloader(object):
@@ -17,54 +18,58 @@ class RTDownloader(object):
     start_time = None
 
     def __init__(self, interval, symbols, special_tags):
+        ''':param symbol: A list of stock symbols uniquely identifying a listed stock
+        :param special_tags: The special tags string, that indicate the data types
+        https://greenido.wordpress.com/2009/12/22/yahoo-finance-hidden-api/
+        for more information.
+        '''
         self.counter = 0
         self.sleep_interval = interval
         self.symbols = symbols
         self.special_tags = special_tags
 
-    def start(self):
-        '''
-        Commence downloading. Downloading will be done in an infinite while loop,
-        requring manual interruption to stop.
-        '''
-        if self.start_time is not None:
-            raise Exception("Downloader already started!")
-        self.start_time = time.time()
 
-        while True:
-            self.download_stock_data()
-            time.sleep(self.sleep_interval)
-
-    def download_stock_data(self):
+    def download_stock_data(self, max_symbols = 400):
         """
         Download the latest stock data for the given symbols, with the given special tags.
         The data is returned in CSV format, where each row is a stock symbol.
         Each special tag becomes a column
-        :param symbol: A list of stock symbols uniquely identifying a listed stock
-        :param special_tags: The special tags string, that indicate the data types
-        https://greenido.wordpress.com/2009/12/22/yahoo-finance-hidden-api/
-        for more information.
+        :param max_symbols: The maximum number of symbols to download at once, necessary to avoid too long URLs
         """
 
-        interval = time.time() - self.start_time
-        print "Downloading round %d. Elapsed time = %f" % (self.counter, interval)
+        steps = len(self.symbols) / max_symbols
+        remainder = len(self.symbols) - steps * max_symbols
+        symbol_chunks = [self.symbols[i*max_symbols:(i+1)*max_symbols] for i in range(steps)]
+
+        # print out
+        symbol_chunks.append(self.symbols[(steps * max_symbols):])
+        chunk = 0
+
         print "Download real-time stocks with special tags '%s'" % (self.special_tags)
+        print "Total chunks = %d of size %d each." % (len(symbol_chunks), len(symbol_chunks[0]))
 
-        # Create a plus sign separated string from all the symbols
-        symbol_string = "+".join(self.symbols)
+        start_time = time.time()
 
-        url = URL_FMT % (symbol_string, self.special_tags)
+        while True:
+            for symbol_chunk in symbol_chunks:
+                print len(symbol_chunk)
+                interval = time.time() - start_time
 
-        output_path = OUTPUT_FILE_FMT % self.counter
+                # Create a plus sign separated string from all the symbols
+                symbol_string = "+".join(symbol_chunk)
+                url = URL_FMT % (symbol_string, self.special_tags)
+                output_path = OUTPUT_FILE_FMT % (self.counter, chunk)
 
-        print "Fetching from '%s'" % (url)
-        print "Writing to file '%s'" % (output_path)
+                # print "Fetching from '%s'" % (url)
+                print "Writing to file '%s'" % (output_path)
 
-        urllib.urlretrieve(url, output_path)
+                urllib.urlretrieve(url, output_path)
 
-        self.counter += 1
+                print "Download round %d / chunk %d finished. Elapsed time = %f" % (self.counter, chunk, interval)
+                chunk += 1
 
-
+                time.sleep(self.sleep_interval)
+            self.counter += 1
 
 def main():
     """
@@ -76,7 +81,8 @@ def main():
     with open(SYMBOL_FILE_NAME, 'r') as symbol_file:
         for line in symbol_file:
             # print line.split('\t')
-            symbols.append(line.split('\t')[0])
+            # symbols.append(line.split('\t')[0])
+            symbols.append(line[:-1])
 
     print "Read %d symbols." % len(symbols)
 
@@ -89,10 +95,13 @@ def main():
     # symbols = ["MSFT", "GOOG", "GE"] # Microsoft, Google and General Electric
 
     # Start downloading data for the symbols at 10 minute intervals
-    # downloader = RTDownloader(6000, symbols, special_tags)
-    # downloader.start()
+    downloader = RTDownloader(6000, symbols, special_tags)
+    downloader.download_stock_data()
 
 
 # When explicitly calling this script
 if __name__ == '__main__':
+    # main()
+
     main()
+
